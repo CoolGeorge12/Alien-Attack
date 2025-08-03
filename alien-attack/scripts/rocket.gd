274c69190ce2ebcd3b6f64452d74ca42b6d450e5
CoolGeorge <zack87608@gmail.com>
2025-08-03 15:47:01 -0600

    no longer frame rate independent and now can shoot rockets

--- alien-attack/scripts/rocket.gd
+++ alien-attack/scripts/rocket.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-@export var speed = 5
+@export var speed = 120
 
 func _physics_process(delta: float) -> void:
-	#global_position.x = global_position.x + 2
-	global_position.x += speed
+	global_position.x += speed*delta
