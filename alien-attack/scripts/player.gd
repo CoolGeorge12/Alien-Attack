90c4824629bb7858f5f2dd02704532ab19c470fa
CoolGeorge <zack87608@gmail.com>
2025-08-03 21:24:48 -0600

    bug fixes

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -4,6 +4,8 @@ var speed = 300
 
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
+@onready var rocket_container = $RocketContainer
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed( "shoot"):
 		shoot()
@@ -28,5 +30,6 @@ func _physics_process(delta: float) -> void:
 
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
