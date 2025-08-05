ad926778481e74989adb4dea5ee71e44d792a3d5
CoolGeorge <zack87608@gmail.com>
2025-08-04 23:36:12 -0600

    Lag Proof

--- alien-attack/scripts/rocket.gd
+++ alien-attack/scripts/rocket.gd
@@ -2,5 +2,14 @@ extends Area2D
 
 @export var speed = 120
 
+@onready var visible_notifier: VisibleOnScreenNotifier2D = $VisibleNotifier
+
+func _ready() -> void:
+	visible_notifier.connect("screen_exited", _on_screen_exited)
+
 func _physics_process(delta: float) -> void:
 	global_position.x += speed*delta
+
+func _on_screen_exited():
+	print("rocket exited screen")
+	queue_free()
