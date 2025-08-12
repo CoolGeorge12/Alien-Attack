9484b98dd2123b323b18f8ce9fd542f55aa893a2
CoolGeorge <zack87608@gmail.com>
2025-08-11 22:22:24 -0600

    game over after player looses 3 lives and score

--- alien-attack/scripts/game.gd
+++ alien-attack/scripts/game.gd
@@ -1,4 +1,29 @@
 extends Node2D
 
+var lives = 3
+var score = 0
+
+@onready var player: CharacterBody2D = $Player
+@onready var hud: Control = $UI/HUD
+
+func _ready() -> void:
+	hud.set_score_label(score)
+
 func _on_deathzone_area_entered(area: Area2D) -> void:
 	area.die()
+
+func _on_player_took_damage() -> void:
+	lives -= 1
+	if (lives == 0):
+		print("Game over")
+		player.die()
+	else:
+		print(lives)
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
+	enemy_instance.connect("died", _on_enemy_died)
+	add_child(enemy_instance)
+
+func _on_enemy_died():
+	score += 100
+	hud.set_score_label(score)
