7fa651029e68f6a4df374fa0e0b06e54cf87968c
CoolGeorge <zack87608@gmail.com>
2025-08-13 19:40:32 -0600

    lives counter

--- alien-attack/scripts/game.gd
+++ alien-attack/scripts/game.gd
@@ -8,17 +8,17 @@ var score = 0
 
 func _ready() -> void:
 	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area: Area2D) -> void:
 	area.die()
 
 func _on_player_took_damage() -> void:
 	lives -= 1
+	hud.set_lives(lives)
 	if (lives == 0):
 		print("Game over")
 		player.die()
-	else:
-		print(lives)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
 	enemy_instance.connect("died", _on_enemy_died)
