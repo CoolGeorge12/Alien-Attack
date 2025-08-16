adb15cd7e67f6cbe3d2adb7e8f2a3de9a3ab2688
CoolGeorge <zack87608@gmail.com>
2025-08-16 12:41:31 -0600

    Game over menu and bug fixes

--- alien-attack/scripts/game.gd
+++ alien-attack/scripts/game.gd
@@ -5,20 +5,30 @@ var score = 0
 
 @onready var player: CharacterBody2D = $Player
 @onready var hud: Control = $UI/HUD
+@onready var ui: CanvasLayer = $UI
+
+var gos_scene = preload("res://scenes/game_over_screen.tscn")
 
 func _ready() -> void:
 	hud.set_score_label(score)
 	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area: Area2D) -> void:
-	area.die()
+	area.queue_free()
 
 func _on_player_took_damage() -> void:
 	lives -= 1
 	hud.set_lives(lives)
+	score = score - 100
 	if (lives == 0):
 		print("Game over")
 		player.die()
+		
+		await get_tree().create_timer(1.5).timeout
+		
+		var gos = gos_scene.instantiate()
+		gos.set_score(score)
+		ui.add_child(gos)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
 	enemy_instance.connect("died", _on_enemy_died)
